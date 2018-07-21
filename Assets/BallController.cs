8bbaa2b19f39e62280efa307b44b072299efe4fc
namekosoba <nameko.oroshi.soba@gmail.com>
2018-07-22 01:22:24 +0900

    Lesson5課題提出

---
+++ Assets/BallController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+        
+        //ボールが見える可能性のあるz軸の最大値
+        private float visiblePosZ = -6.5f;
+
+        //ゲームオーバを表示するテキスト
+        private GameObject gameoverText;
+
+        //スコアを表示するテキスト
+        private GameObject scoreText;
+
+        //スコアを初期化
+        private int score = 0;
+
+        // Use this for initialization
+        void Start () {
+                //シーン中のGameOverTextオブジェクトを取得
+                this.gameoverText = GameObject.Find("GameOverText");
+
+                //シーン中のScoreTextオブジェクトを取得
+                this.scoreText = GameObject.Find("ScoreText");
+
+        }
+        
+        // Update is called once per frame
+        void Update () {
+        		//スコアを表示
+     	       scoreText.GetComponent<Text> ().text = "Score:" + score;
+
+                //ボールが画面外に出た場合
+                if (this.transform.position.z < this.visiblePosZ) {
+                        //GameoverTextにゲームオーバを表示
+                        this.gameoverText.GetComponent<Text> ().text = "GAME OVER";
+                }
+        }
+
+        //衝突時に呼ばれる関数
+        void OnCollisionEnter(Collision other) {
+
+        		//大きい星に当たった時：+30点
+        		if (other.gameObject.tag == "LargeStarTag"){
+        			score += 30;
+
+    	        //小さい星に当たった時：+5点
+    	        }else if(other.gameObject.tag == "SmallStarTag"){
+        			score += 5;
+
+    	        //大きい雲に当たった時：+15点
+    	        }else if(other.gameObject.tag == "LargeCloudTag"){
+        			score += 15;
+    	        
+    	        //小さい雲に当たった時：+10点
+    	        }else if(other.gameObject.tag == "SmallCloudTag"){
+        			score += 10;
+    	        }
+    	}
+}
---
+++ Assets/FripperController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FripperController : MonoBehaviour {
+        //HingiJointコンポーネントを入れる
+        private HingeJoint myHingeJoint;
+
+        //初期の傾き
+        private float defaultAngle = 20;
+        //弾いた時の傾き
+        private float flickAngle = -20;
+
+        // Use this for initialization
+        void Start () {
+                //HingeJointコンポーネント取得
+                this.myHingeJoint = GetComponent<HingeJoint>();
+
+                //フリッパーの傾きを設定
+                SetAngle(this.defaultAngle);
+
+        }
+
+        // Update is called once per frame
+        void Update () {
+                //タッチの検出
+                if(Input.touchCount > 0){
+
+                        for(int i = 0; i < Input.touchCount; i++){
+                                
+                                //タッチ情報の取得
+                                Touch touch = Input.GetTouch(i);
+
+                                //タッチした直後
+                                if(touch.phase == TouchPhase.Began){
+
+                                        //画面の右半分をタップした時、右フリッパーを動かす
+                                        if (Input.mousePosition.x >= Screen.width / 2 && tag == "RightFripperTag") {
+                                                SetAngle (this.flickAngle);
+
+                                        //画面の左半分をタップした時、左フリッパーを動かす
+                                        } else if (Input.mousePosition.x < Screen.width / 2 && tag == "LeftFripperTag") {
+                                                SetAngle (this.flickAngle);
+                                        }
+
+                                //画面から指が離れたらフリッパーを元に戻す
+                                } else if (touch.phase == TouchPhase.Ended){
+                                        SetAngle (this.defaultAngle);
+                                }
+                        }
+                }
+        }
+
+
+        //フリッパーの傾きを設定
+        public void SetAngle (float angle){
+                JointSpring jointSpr = this.myHingeJoint.spring;
+                jointSpr.targetPosition = angle;
+                this.myHingeJoint.spring = jointSpr;
+        }
+}
